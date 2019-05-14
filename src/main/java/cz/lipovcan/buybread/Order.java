eb549684cf592f1bbfb35edfff726598da8bc991
wbadmin <>
2019-05-14 16:50:41 +0000

    {/src/main/java/cz/lipovcan/buybread/Order.java}

--- src/main/java/cz/lipovcan/buybread/Order.java
+++ src/main/java/cz/lipovcan/buybread/Order.java
@@ -7,29 +7,38 @@ package cz.lipovcan.buybread;
 @javax.persistence.Entity
 public class Order implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(generator = "ORDER_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "ORDER_ID_SEQ", name = "ORDER_ID_GENERATOR")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(generator = "ORDER_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(sequenceName = "ORDER_ID_SEQ", name = "ORDER_ID_GENERATOR")
+	private java.lang.Long id;
 
-    public Order() {
-    }
-    
-    public Order(java.lang.Long id) {
-        this.id = id;
-    }
+	@javax.persistence.ManyToOne(cascade = {javax.persistence.CascadeType.ALL}, fetch = javax.persistence.FetchType.EAGER)
+	private cz.lipovcan.buybread.Product product;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public Order() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
+	public cz.lipovcan.buybread.Product getProduct() {
+		return this.product;
+	}
+
+	public void setProduct(cz.lipovcan.buybread.Product product) {
+		this.product = product;
+	}
+
+	public Order(java.lang.Long id, cz.lipovcan.buybread.Product product) {
+		this.id = id;
+		this.product = product;
+	}
 
 }
