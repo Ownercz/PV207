ddfd53af43dc36720faf4595b7f29c161a536e4a
wbadmin <>
2019-05-14 19:39:11 +0000

    {/src/main/java/cz/lipovcan/buybread/Warehouse_Database.java}

--- src/main/java/cz/lipovcan/buybread/Warehouse_Database.java
+++ src/main/java/cz/lipovcan/buybread/Warehouse_Database.java
@@ -7,29 +7,37 @@ package cz.lipovcan.buybread;
 @javax.persistence.Entity
 public class Warehouse_Database implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "WAREHOUSE_DATABASE_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "WAREHOUSE_DATABASE_ID_SEQ", name = "WAREHOUSE_DATABASE_ID_GENERATOR")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "WAREHOUSE_DATABASE_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(sequenceName = "WAREHOUSE_DATABASE_ID_SEQ", name = "WAREHOUSE_DATABASE_ID_GENERATOR")
+	private java.lang.Long id;
 
-    public Warehouse_Database() {
-    }
-    
-    public Warehouse_Database(java.lang.Long id) {
-        this.id = id;
-    }
+	private java.lang.Integer amount;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public Warehouse_Database() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
+	public java.lang.Integer getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(java.lang.Integer amount) {
+		this.amount = amount;
+	}
+
+	public Warehouse_Database(java.lang.Long id, java.lang.Integer amount) {
+		this.id = id;
+		this.amount = amount;
+	}
 
 }
