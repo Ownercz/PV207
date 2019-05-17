69f8844fa6cac76fd88768eb8e5b2d8e7ba329d5
wbadmin <>
2019-05-17 16:57:45 +0000

    {/src/main/java/cz/lipovcan/buybread/PromotionInventory.java}

--- src/main/java/cz/lipovcan/buybread/PromotionInventory.java
+++ src/main/java/cz/lipovcan/buybread/PromotionInventory.java
@@ -12,6 +12,14 @@ public class PromotionInventory implements java.io.Serializable {
 
 	private int daysToExpire;
 
+	private int id;
+
+	private java.lang.String name;
+
+	private int amount;
+
+	private int price;
+
 	public PromotionInventory() {
 	}
 
@@ -23,8 +31,45 @@ public class PromotionInventory implements java.io.Serializable {
 		this.daysToExpire = daysToExpire;
 	}
 
-	public PromotionInventory(int daysToExpire) {
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public int getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public PromotionInventory(int daysToExpire, int id, java.lang.String name,
+			int amount, int price) {
 		this.daysToExpire = daysToExpire;
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+		this.price = price;
 	}
 
 }
